76f78aee68529988d2948562c0168ce5f05a7513
Mashrur Hossain <bddrailsudemy@gmail.com>
2016-10-22 19:44:33 +0000

    Add flash.now to article create failure path

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -12,7 +12,7 @@ class ArticlesController < ApplicationController
       flash[:success] = "Article has been created"
       redirect_to articles_path
     else
-      flash[:danger] = "Article has not been created"
+      flash.now[:danger] = "Article has not been created"
       render :new
     end
   end
