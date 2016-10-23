5fb485895f93d100860626667067c4c154ffce1c
Mashrur Hossain <bddrailsudemy@gmail.com>
2016-10-23 18:29:10 +0000

    Fix broken editing article spec

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,5 @@
 class ArticlesController < ApplicationController
-  #before_action :authenticate_user!, except: [:index, :show]
+  before_action :authenticate_user!, except: [:index, :show]
   before_action :set_article, only: [:show, :edit, :update, :destroy]
   
   def index
--- spec/features/editing_article_spec.rb
+++ spec/features/editing_article_spec.rb
@@ -3,7 +3,9 @@ require 'rails_helper'
 RSpec.feature "Editing an article" do
   
   before do
-    @article = Article.create(title: "Title One", body: "Body of article one")
+    john = User.create(email: "john@example.com", password: "password")
+    login_as(john)
+    @article = Article.create(title: "Title One", body: "Body of article one", user: john)
   end
   
   scenario "A user updates an article" do
