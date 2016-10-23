126d377f5dacb945be9708308a736b7d2915f873
Mashrur Hossain <bddrailsudemy@gmail.com>
2016-10-23 23:36:47 +0000

    Hide links from non-signed in users and non-owners

--- spec/features/show_article_spec.rb
+++ spec/features/show_article_spec.rb
@@ -3,18 +3,33 @@ require "rails_helper"
 RSpec.feature "Showing an article" do
   
   before do
-    john = User.create(email: "john@example.com", password: "password")
-    login_as(john)
-    @article = Article.create(title: "The first article", body: "Lorem ipsum dolor sit amet, consectetur.", user: john)
+    @john = User.create(email: "john@example.com", password: "password")
+    @fred = User.create(email: "fred@example.com", password: "password")
+    @article = Article.create(title: "The first article", body: "Lorem ipsum dolor sit amet, consectetur.", user: @john)
   end
   
-  scenario "A user shows an article" do
+  scenario "to non-signed in user hide the Edit and Delete buttons" do
     visit "/"
     click_link @article.title
     
     expect(page).to have_content(@article.title)
     expect(page).to have_content(@article.body)
     expect(current_path).to eq(article_path(@article))
+    
+    expect(page).not_to have_link("Edit Article")
+    expect(page).not_to have_link("Delete Article")
   end
   
+   scenario "to non-owner hide the Edit and Delete buttons" do
+    login_as(@fred)
+    visit "/"
+    click_link @article.title
+    
+    expect(page).to have_content(@article.title)
+    expect(page).to have_content(@article.body)
+    expect(current_path).to eq(article_path(@article))
+    
+    expect(page).not_to have_link("Edit Article")
+    expect(page).not_to have_link("Delete Article")
+  end
 end
